0a85a01be3fc69c63e097e789b66d44ca6b5346a
Gosia Pułka <malgosia.pulka@gmail.com>
2023-09-14 09:06:50 +0200

    dodanie pierwszych programów w pytonie

---
+++ NaukaPython/gui.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Kot")
+root.geometry("800x480")
---
+++ NaukaPython/main.py
@@ -0,0 +1,16 @@
+
+imie = input("Jak masz na imię?")
+
+duze_imie = imie.upper()
+
+print("Cześć,", duze_imie)
+
+liczbaX = input("Podaj pierwszą liczbę?")
+liczbaY = input("Podaj drugą liczbę?")
+liczbaZ = input("Podaj trzecią liczbę?")
+
+liczbaX = int(liczbaX)
+liczbaY = int(liczbaY)
+liczbaZ = int(liczbaZ)
+print("Suma tych liczb to: ", liczbaX+liczbaY+liczbaZ)
+
